b98d24feafd76af4eedb0af10feeb1420e58d3e4
Ken <okalang.ds@gmail.com>
2021-10-11 13:01:04 +0200

    Add project files.

---
+++ ParserTest/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ParserTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                SimpleParserTests.TestReturnsZeroWhenEmptyString();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+        }
+    }
+}
---
+++ ParserTest/SimplePaser.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+public class SimplePaser
+    {
+        public int ParseAndSum(string numbers)
+        {
+            if (numbers.Length == 0)
+            {
+                return 0;
+            }
+            if (!numbers.Contains(","))
+            {
+                return int.Parse(numbers);
+            }
+            else
+            {
+                "I can only handle 0 or 1 numbers for now!");
+            }
+        }
+    }
+
+
+class SimpleParserTests
+{
+    public static void TestReturnsZeroWhenEmptyString()
+    {
+        try
+        {
+            SimplePaser p = new SimplePaser();
+            int result = p.ParseAndSum(string.Empty);
+            if(result != 0)
+            {
+                Console.WriteLine(
+                    @"***SimpleParserTests.TestReturnZeroWhenEmptyString:
+                    -------
+                    Parser and sum should have returned 0 on an empty string");
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+        }
+    }
+}
